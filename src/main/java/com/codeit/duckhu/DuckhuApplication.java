91ed4178a1fecaf29ba3598d8f32a612dc1b7f75
normaldeve <junnukim1007@gmail.com>
2025-04-17 00:03:54 +0900

    Initial commit: Spring Boot project

---
+++ src/main/java/com/codeit/duckhu/DuckhuApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.duckhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckhuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DuckhuApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codeit/duckhu/DuckhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.duckhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckhuApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

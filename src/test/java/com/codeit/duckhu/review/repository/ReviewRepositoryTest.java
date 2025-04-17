2716ded53d36e2175cc297401918376352394ec0
byeonheejae <pion0458@gmail.com>
2025-04-17 16:50:05 +0900

    feat/3 TDD repository red

---
+++ src/test/java/com/codeit/duckhu/review/repository/ReviewRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.codeit.duckhu.review.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class ReviewRepositoryTest {
+
+  @Autowired
+  private TestEntityManager em;
+
+
+  @Autowired
+  private ReviewRepository repo;
+
+  @Test
+  @DisplayName("리뷰 저장 확인")
+  void saveReview_() {
+    // Given
+    Review savedReview = Review.builder
+            .rating(5)
+            .content("재밌어요")
+            .likeCount(0)
+            .commentCount(0)
+            .likeByMe(false)
+            .build;
+
+    // When
+    repo.save(savedReview);
+    em.flush();
+    em.clear();
+
+    // Then
+    Review foundReview = em.find(Review.class, savedReview.getId());
+
+    assertThat(foundReview).isNotNull();
+    assertThat(foundReview.getRating()).isEqualTo(5);
+    assertThat(foundReview.getContent()).isEqualTo("재밌어요");
+    assertThat(foundReview.getLikeCount()).isEqualTo(0);
+    assertThat(foundReview.getCommentCount()).isEqualTo(0);
+    assertThat(foundReview.getLikeByMe()).isEqualTo(false);
+  }
+
+
+}

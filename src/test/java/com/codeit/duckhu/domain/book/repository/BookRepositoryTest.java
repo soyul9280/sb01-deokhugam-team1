f50da04d49af5684e888d97738fb134ac323f667
normaldeve <junnukim1007@gmail.com>
2025-04-18 09:40:52 +0900

    :fire: Github workflow 제거 - applicatoin.yml 파일을 제외한 나머지 파일을 gitignore를 하다보니 테스트 진행이 안되어 제거하였습니다

--- src/test/java/com/codeit/duckhu/domain/book/repository/BookRepositoryTest.java
+++ src/test/java/com/codeit/duckhu/domain/book/repository/BookRepositoryTest.java
@@ -11,16 +11,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
 @Import(AuditingConfig.class)
-@Transactional
 class BookRepositoryTest {
 
   @Autowired

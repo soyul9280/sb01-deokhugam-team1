b41444fcfe0df056101a8caa75c1c0d3f76983e9
normaldeve <junnukim1007@gmail.com>
2025-04-18 09:26:57 +0900

    :fire: PopularBook 엔티티 제외 - DB에서 period_type에 대해 고민 중

--- src/main/java/com/codeit/duckhu/domain/book/entity/Book.java
+++ src/main/java/com/codeit/duckhu/domain/book/entity/Book.java
@@ -47,8 +47,4 @@ public class Book extends BaseUpdatableEntity {
   @Builder.Default
   @Column(name = "is_deleted", nullable = false)
   private Boolean isDeleted = false;
-
-  @Builder.Default
-  @OneToMany(mappedBy = "book", cascade = CascadeType.REMOVE, orphanRemoval = true)
-  private List<PopularBook> popularBooks = new ArrayList<>();
 }
--- src/main/java/com/codeit/duckhu/global/type/PeriodType.java
+++
@@ -1,9 +0,0 @@
-package com.codeit.duckhu.global.type;
-
-public enum PeriodType {
-  DAILY,
-  WEEKLY,
-  MONTHLY,
-  ALL_TIME
-}
-

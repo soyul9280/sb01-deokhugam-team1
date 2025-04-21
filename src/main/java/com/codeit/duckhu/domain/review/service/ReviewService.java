5a1b8bbc7840359e4f6842b0c896178fbb354719
byeonheejae <pion0458@gmail.com>
2025-04-21 17:51:22 +0900

    feat: ReviewLike

--- src/main/java/com/codeit/duckhu/domain/review/service/ReviewService.java
+++ src/main/java/com/codeit/duckhu/domain/review/service/ReviewService.java
@@ -2,19 +2,23 @@ package com.codeit.duckhu.domain.review.service;
 
 import com.codeit.duckhu.domain.review.dto.ReviewCreateRequest;
 import com.codeit.duckhu.domain.review.dto.ReviewDto;
+import com.codeit.duckhu.domain.review.dto.ReviewLikeDto;
 import com.codeit.duckhu.domain.review.dto.ReviewUpdateRequest;
+import com.codeit.duckhu.domain.review.entity.Review;
 import java.util.UUID;
 
 /**
  * 리뷰 서비스 인터페이스
  */
 public interface ReviewService {
-  
+
   ReviewDto createReview(ReviewCreateRequest request);
-  
+
   ReviewDto getReviewById(UUID id);
-  
+
   void deleteReviewById(UUID id);
 
   ReviewDto updateReview(UUID id, ReviewUpdateRequest request);
+
+  ReviewLikeDto likeReview(UUID reviewId, UUID userId);
 }

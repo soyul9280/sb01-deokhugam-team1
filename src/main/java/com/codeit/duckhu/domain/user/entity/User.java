26829768bf9c252aa675952eb8c4391ffdf8f544
byeonheejae <pion0458@gmail.com>
2025-04-21 09:39:34 +0900

    연관관계 매핑

--- src/main/java/com/codeit/duckhu/domain/user/entity/User.java
+++ src/main/java/com/codeit/duckhu/domain/user/entity/User.java
@@ -1,9 +1,13 @@
 package com.codeit.duckhu.domain.user.entity;
 
+import com.codeit.duckhu.domain.review.entity.Review;
 import com.codeit.duckhu.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +31,9 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private boolean isDeleted;
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> review = new ArrayList<>();
 
     public User(String email, String nickname, String password, boolean isDeleted) {
         this.email = email;

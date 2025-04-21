3705da5e60b81d1b0ae98f7691bbd27797ce236f
GiMin0123 <minby260306@gmail.com>
2025-04-21 17:46:59 +0900

    feat: 알림 확인 및 전체 읽음 처리용 NotificationController 추가

--- src/main/java/com/codeit/duckhu/domain/notification/repository/NotificationRepsitory.java
+++ src/main/java/com/codeit/duckhu/domain/notification/repository/NotificationRepsitory.java
@@ -20,11 +20,4 @@ public interface NotificationRepsitory extends JpaRepository<Notification, UUID>
      */
     List<Notification> findAllByReceiverId(UUID receiverId);
 
-    /**
-     * 특정 사용자의 아직 읽지 않은(confirmed = false) 알림만 조회합니다.
-     *
-     * @param receiverId 알림 수신자 ID
-     * @return 미확인 알림 리스트
-     */
-    List<Notification> findAllByReceiverIdAndConfirmedIsFalse(UUID receiverId);
 }

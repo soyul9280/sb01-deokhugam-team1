336ce048430bbd9df2f2a97b61115892af94224c
byeonheejae <pion0458@gmail.com>
2025-04-21 14:25:18 +0900

    Review like test

--- src/test/java/com/codeit/duckhu/domain/comments/CommentControllerTest.java
+++ src/test/java/com/codeit/duckhu/domain/comments/CommentControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;

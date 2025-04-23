8c91714482e539d3f9e5b99d3da6faa06b87f512
byeonheejae <pion0458@gmail.com>
2025-04-23 11:31:13 +0900

    controller nested

--- src/test/java/com/codeit/duckhu/domain/review/controller/ReviewControllerTest.java
+++ src/test/java/com/codeit/duckhu/domain/review/controller/ReviewControllerTest.java
@@ -16,6 +16,7 @@ import com.codeit.duckhu.global.exception.GlobalExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -37,71 +38,81 @@ public class ReviewControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  @Test
-  @DisplayName("POST /api/reviews-성공")
-  void createReview_Success() throws Exception {
-    // Given
-    ReviewCreateRequest request = ReviewCreateRequest.builder()
-        .userId(UUID.randomUUID())
-        .bookId(UUID.randomUUID())
-        .rating(5)
-        .content("재밌어요 !")
-        .build();
 
-    ReviewDto review = ReviewDto.builder()
-        .id(UUID.randomUUID())
-        .userId(request.getUserId())
-        .bookId(request.getBookId())
-        .rating(request.getRating())
-        .content(request.getContent())
-        .createdAt(null)
-        .updatedAt(null)
-        .build();
+  @Nested
+  @DisplayName("리뷰 생성 테스트")
+  class createReview {
 
-    given(reviewService.createReview(any(ReviewCreateRequest.class)))
-        .willReturn(review);
+    @Test
+    @DisplayName("POST /api/reviews-성공")
+    void createReview_Success() throws Exception {
+      // Given
+      ReviewCreateRequest request = ReviewCreateRequest.builder()
+          .userId(UUID.randomUUID())
+          .bookId(UUID.randomUUID())
+          .rating(5)
+          .content("재밌어요 !")
+          .build();
 
-    mockMvc.perform(post("/api/reviews")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isCreated())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.id").value(review.getId().toString()))
-        .andExpect(jsonPath("$.userId").value(review.getUserId().toString()))
-        .andExpect(jsonPath("$.bookId").value(review.getBookId().toString()))
-        .andExpect(jsonPath("$.rating").value(review.getRating()))
-        .andExpect(jsonPath("$.content").value(review.getContent()));
+      ReviewDto review = ReviewDto.builder()
+          .id(UUID.randomUUID())
+          .userId(request.getUserId())
+          .bookId(request.getBookId())
+          .rating(request.getRating())
+          .content(request.getContent())
+          .createdAt(null)
+          .updatedAt(null)
+          .build();
+
+      given(reviewService.createReview(any(ReviewCreateRequest.class)))
+          .willReturn(review);
+
+      mockMvc.perform(post("/api/reviews")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request)))
+          .andExpect(status().isCreated())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("$.id").value(review.getId().toString()))
+          .andExpect(jsonPath("$.userId").value(review.getUserId().toString()))
+          .andExpect(jsonPath("$.bookId").value(review.getBookId().toString()))
+          .andExpect(jsonPath("$.rating").value(review.getRating()))
+          .andExpect(jsonPath("$.content").value(review.getContent()));
+    }
   }
 
-  @Test
-  @DisplayName("PATCH /api/reviews-성공")
-  void updateReview_Success() throws Exception{
-    UUID reviewId = UUID.randomUUID();
+  @Nested
+  @DisplayName("리뷰 수정 테스트")
+  class updateReview {
+    @Test
+    @DisplayName("PATCH /api/reviews-성공")
+    void updateReview_Success() throws Exception{
+      UUID reviewId = UUID.randomUUID();
 
-    ReviewUpdateRequest request = ReviewUpdateRequest.builder()
-        .userId(UUID.randomUUID())
-        .bookId(UUID.randomUUID())
-        .rating(4)
-        .content("볼만해요 !")
-        .build();
+      ReviewUpdateRequest request = ReviewUpdateRequest.builder()
+          .userId(UUID.randomUUID())
+          .bookId(UUID.randomUUID())
+          .rating(4)
+          .content("볼만해요 !")
+          .build();
 
-    ReviewDto review = ReviewDto.builder()
-        .id(reviewId)
-        .userId(UUID.randomUUID())
-        .bookId(UUID.randomUUID())
-        .rating(request.getRating())
-        .content(request.getContent())
-        .build();
+      ReviewDto review = ReviewDto.builder()
+          .id(reviewId)
+          .userId(UUID.randomUUID())
+          .bookId(UUID.randomUUID())
+          .rating(request.getRating())
+          .content(request.getContent())
+          .build();
 
-    given(reviewService.updateReview(any(UUID.class), any(ReviewUpdateRequest.class)))
-        .willReturn(review);
+      given(reviewService.updateReview(any(UUID.class), any(ReviewUpdateRequest.class)))
+          .willReturn(review);
 
-    mockMvc.perform(patch("/api/reviews/{reviewId}", reviewId)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.rating").value(request.getRating()))
-        .andExpect(jsonPath("$.content").value(request.getContent()));
+      mockMvc.perform(patch("/api/reviews/{reviewId}", reviewId)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request)))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.rating").value(request.getRating()))
+          .andExpect(jsonPath("$.content").value(request.getContent()));
+    }
   }
 
 }

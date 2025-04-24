b9773b3ca9596dd2c4ea3caacc908e2f8f58186a
byeonheejae <pion0458@gmail.com>
2025-04-24 13:37:59 +0900

    PopularReview Repository 생성

---
+++ src/main/java/com/codeit/duckhu/domain/review/repository/PopularReviewRepository.java
@@ -0,0 +1,11 @@
+package com.codeit.duckhu.domain.review.repository;
+
+import com.codeit.duckhu.domain.review.entity.PopularReview;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PopularReviewRepository extends JpaRepository<PopularReview, UUID> {
+
+}

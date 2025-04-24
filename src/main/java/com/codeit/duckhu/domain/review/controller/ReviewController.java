3103c99d8e60662619cc028fb08526168a1b5d19
byeonheejae <pion0458@gmail.com>
2025-04-24 11:33:51 +0900

    likeReview RequestParam -> RequestHearder

--- src/main/java/com/codeit/duckhu/domain/review/controller/ReviewController.java
+++ src/main/java/com/codeit/duckhu/domain/review/controller/ReviewController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class ReviewController {
   @PostMapping("/{reviewId}/like")
   public ResponseEntity<ReviewLikeDto> likeReview(
       @PathVariable("reviewId") UUID reviewId,
-      @RequestParam("userId") UUID userId) {
+      @RequestHeader("X-USER-ID") UUID userId) {
     ReviewLikeDto result = reviewService.likeReview(reviewId, userId);
     return ResponseEntity.ok(result);
   }
